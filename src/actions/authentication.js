85a6d77679790a4293f1ccaee2121519877fc2bc
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-19 17:03:31 -0500

    Implement login actions creator in authentication

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -3,6 +3,8 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   SET_MESSAGE,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
 } from './types';
 
 import AuthenticationService from '../services/authentication.service';
@@ -36,4 +38,27 @@ export const register = (username, email, password) => (dispatch) => Authenticat
   },
 );
 
-export default { register };
+export const login = (username, email, password) => (dispatch) => AuthenticationService.login(username, email, password).then(
+  (data) => {
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: { user: data },
+    });
+
+    return Promise.resolve();
+  },
+  (error) => {
+    const message = error;
+
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+
+    dispatch({
+      type: SET_MESSAGE,
+      payload: message,
+    });
+
+    return Promise.reject();
+  },
+);
