c3ed246874f4e8ee7c22156db3d966eb9903df23
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-19 13:56:03 -0500

    implement register a user method

--- src/services/authentication.service.js
+++ src/services/authentication.service.js
@@ -2,3 +2,12 @@ import axios from "axios";
 import API_root from "../constantes/api";
 
 const API_URL = API_root;
+
+const register = (username, email, password) => {
+  return axios.post(API_URL + "signup", {
+    username,
+    email,
+    password,
+  });
+};
+
