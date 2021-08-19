09f862151ab2f0bb407d6eaaef551380b03cc60d
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-19 14:03:04 -0500

    Implement login method in autentication service

--- src/services/authentication.service.js
+++ src/services/authentication.service.js
@@ -11,3 +11,19 @@ const register = (username, email, password) => {
   });
 };
 
+const login = (username, email, password) => {
+  return axios
+    .post(API_URL + "login", {
+      username,
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
