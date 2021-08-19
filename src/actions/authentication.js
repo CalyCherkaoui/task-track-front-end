1cdeff5f795c382bfc17a7929bf16e81a8428afb
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-19 16:59:32 -0500

    Add message handleing to register action creator

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -1,6 +1,8 @@
+/* eslint-disable max-len */
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
+  SET_MESSAGE,
 } from './types';
 
 import AuthenticationService from '../services/authentication.service';
@@ -11,24 +13,27 @@ export const register = (username, email, password) => (dispatch) => Authenticat
       type: REGISTER_SUCCESS,
     });
 
+    dispatch({
+      type: SET_MESSAGE,
+      payload: response.data.message, // verify how errors are handled in the backend
+    });
+
     return Promise.resolve();
   },
   (error) => {
-    const message = (error.response
-          && error.response.data
-          && error.response.data.message)
-        || error.detail
-        || error.toString(); // verify this with backend responses
+    const message = error; // verify how errors are handled in the backend
 
     dispatch({
       type: REGISTER_FAIL,
     });
 
     dispatch({
-      // type: MESSAGE, type set_message to be implemneted
+      type: SET_MESSAGE,
       payload: message,
     });
 
     return Promise.reject();
   },
 );
+
+export default { register };
