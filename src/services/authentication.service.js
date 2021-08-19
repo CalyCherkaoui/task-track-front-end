59a867f7e96ead10d6271365d3579787fd5905ee
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-19 15:00:48 -0500

    Fix linters errors

--- src/services/authentication.service.js
+++ src/services/authentication.service.js
@@ -1,29 +1,24 @@
-import axios from "axios";
-import API_root from "../constantes/api";
+import axios from 'axios';
+import API_ROOT from '../constantes/api';
 
-const API_URL = API_root;
+const API_URL = API_ROOT;
 
-const register = (username, email, password) => {
-  return axios.post(API_URL + "signup", {
+const register = (username, email, password) => axios.post(`${API_URL}signup`, {
+  username,
+  email,
+  password,
+});
+
+const login = (username, email, password) => axios
+  .post(`${API_URL}login`, {
     username,
     email,
     password,
+  })
+  .then((response) => {
+    if (response.data.accessToken) {
+      localStorage.setItem('user', JSON.stringify(response.data));
+    }
+
+    return response.data;
   });
-};
-
-const login = (username, email, password) => {
-  return axios
-    .post(API_URL + "login", {
-      username,
-      email,
-      password,
-    })
-    .then((response) => {
-      if (response.data.accessToken) {
-        localStorage.setItem("user", JSON.stringify(response.data));
-      }
-
-      return response.data;
-    });
-};
-
