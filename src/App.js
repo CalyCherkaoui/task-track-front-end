179ff1c6601d3c7f2463a19d9b8761eb67f75f7e
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-20 15:30:35 -0500

    Fix routing user access

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './component/Navigation';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import About from './pages/About';
 import Admin from './pages/Admin';
 import history from './helpers/history';
 
-const App = () => (
-  <Router history={history}>
-    <Navigation />
-    <div className="container mt-3">
-      <Switch>
-        <Route exact path={['/', '/home']} component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/admin" component={Admin} />
-      </Switch>
-    </div>
-  </Router>
-);
+const App = () => {
+  const { user: currentUser } = useSelector((state) => state.authentication);
+  // const { isLoggedIn } = useSelector((state) => state.authentication);
+
+  return (
+    <Router history={history}>
+      <Navigation />
+      <div className="container mt-3">
+        <Switch>
+          {
+            currentUser ? (
+              <Route exact path={['/', '/home']} component={Home} />
+            ) : (
+              <Route exact path={['/', '/about']} component={About} />
+            )
+          }
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/admin" component={Admin} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
