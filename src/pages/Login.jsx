e8e41eedf24e587c06b06ff0914c4b8fd8a8bbd9
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-20 09:45:32 -0500

    Refactor login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import CheckButton from 'react-validation/build/button';
+import PropTypes from 'prop-types';
 import { login } from '../actions/authentication';
+// eslint-disable-next-line consistent-return
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        `Required to login!`
+      </div>
+    );
+  }
+};
 
 const Login = (props) => {
+  const form = useRef();
+  const checkBtn = useRef();
+
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const { isLoggedIn } = useSelector((state) => state.authentication);
   const { message } = useSelector((state) => state.message);
@@ -29,56 +50,109 @@ const Login = (props) => {
 
   const handleLogin = (e) => {
     e.preventDefault();
-// dispatch
     setLoading(true);
+    form.current.validateAll();
+    // eslint-disable-next-line no-underscore-dangle
+    if (checkBtn.current.context._errors.length === 0) {
+      dispatch(login(username, email, password))
+        .then(() => {
+          props.history.push('/profile');
+          window.location.reload();
+        })
+        .catch(() => {
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
+    }
   };
+
+  if (isLoggedIn) {
+    return <Redirect to="/profile" />;
+  }
 
   return (
     <div className="col-md-12">
       <div className="card card-container">
-        <Form onSubmit={handleLogin}>
+        <img
+          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+          alt="profile-img"
+          className="profile-img-card"
+        />
+
+        <Form onSubmit={handleLogin} ref={form}>
           <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <Input
-              type="text"
-              className="form-control"
-              name="username"
-              value={username}
-              onChange={onChangeUsername}
-            />
+            <label htmlFor="username">
+              Username:
+              <Input
+                id="username"
+                type="text"
+                className="form-control"
+                name="username"
+                value={username}
+                onChange={onChangeUsername}
+                validations={[required]}
+              />
+            </label>
           </div>
 
           <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <Input
-              type="Email"
-              className="form-control"
-              name="Email"
-              value={Email}
-              onChange={onChangeEmail}
-            />
+            <label htmlFor="email">
+              Email:
+              <Input
+                id="email"
+                type="text"
+                className="form-control"
+                name="email"
+                value={email}
+                onChange={onChangeEmail}
+                validations={[required]}
+              />
+            </label>
           </div>
 
           <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <Input
-              type="password"
-              className="form-control"
-              name="password"
-              value={password}
-              onChange={onChangePassword}
-            />
+            <label htmlFor="password">
+              Password:
+              <Input
+                id="password"
+                type="password"
+                className="form-control"
+                name="password"
+                value={password}
+                onChange={onChangePassword}
+                validations={[required]}
+              />
+            </label>
           </div>
 
           <div className="form-group">
-            <button>
+            <button className="btn btn-primary btn-block" disabled={loading} type="submit">
+              {loading && (
+                <span className="spinner-border spinner-border-sm" />
+              )}
               <span>Login</span>
             </button>
           </div>
+
+          {message && (
+            <div className="form-group">
+              <div className="alert alert-danger" role="alert">
+                {message}
+              </div>
+            </div>
+          )}
+          <CheckButton style={{ display: 'none' }} ref={checkBtn} />
         </Form>
       </div>
     </div>
   );
+};
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default Login;
