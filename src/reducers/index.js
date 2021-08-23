bde88306b47029ed45554035bb72cdeef29598c4
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-23 16:01:06 -0500

    Update root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authentication from './authentication';
 import message from './message';
+import profile from './profile';
 
 const rootReducer = combineReducers({
   authentication,
   message,
+  profile,
 });
 
 export default rootReducer;
