0293de5e2ac7770b8551d9352b6a9432106eafc9
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-25 16:35:11 -0500

    Refactor home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-const Home = () => (
-  <div className="container">
-    Homeeeee!!!!!
-  </div>
-);
+const Home = () => {
+  const { user: currentUser } = useSelector((state) => state.authentication);
+  if (!currentUser || !sessionStorage.getItem('token')) {
+    return <Redirect to="/login" />;
+    // return (
+    //   history.push('/login')
+    // );
+  }
+
+  return (
+    (
+      <div className="container">
+        Homeeeee!!!!!
+      </div>
+    )
+  );
+};
 
 export default Home;
