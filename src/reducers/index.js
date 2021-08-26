06ad77ef53198cfcd7504e3b819cbdbcede94105
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-26 14:22:57 -0500

    Combine routines with reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import authentication from './authentication';
 import message from './message';
 import profile from './profile';
+import routines from './routines';
 
 const rootReducer = combineReducers({
   authentication,
   message,
   profile,
+  routines,
 });
 
 export default rootReducer;
