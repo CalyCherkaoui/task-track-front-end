8d0c7d66f7a85e58834fd4ec2332fd207a7233f3
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-27 13:00:31 -0500

    Implement task reducer

---
+++ src/reducers/task.js
@@ -0,0 +1,59 @@
+import {
+  GET_TASK_SUCCESS,
+  GET_TASK_FAIL,
+  SET_TASK_SUCCESS,
+  SET_TASK_FAIL,
+  UPDATE_TASK_SUCCESS,
+  UPDATE_TASK_FAIL,
+} from '../actions/types';
+
+const initialState = {
+  task: {},
+  loading: true,
+  message: '',
+};
+
+const taskReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_TASK_SUCCESS:
+      return {
+        ...state,
+        task: action.payload.data,
+        loading: false,
+      };
+    case GET_TASK_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    case SET_TASK_SUCCESS:
+      return {
+        ...state,
+        message: 'Your task was successfully added!',
+        loading: false,
+      };
+    case SET_TASK_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    case UPDATE_TASK_SUCCESS:
+      return {
+        ...state,
+        message: 'Your task was successfully updated!',
+        loading: false,
+      };
+    case UPDATE_TASK_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default taskReducer;
