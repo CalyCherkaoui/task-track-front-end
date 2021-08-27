ce917fa8729ce73a1e04aa67e5cf92ff88070b22
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-27 12:50:17 -0500

    Implement update task method

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -8,6 +8,8 @@ import {
   GET_TASK_FAIL,
   SET_TASK_SUCCESS,
   SET_TASK_FAIL,
+  UPDATE_TASK_SUCCESS,
+  UPDATE_TASK_FAIL,
 } from './types';
 
 export const getTask = (taskid) => async (dispatch) => {
@@ -19,7 +21,7 @@ export const getTask = (taskid) => async (dispatch) => {
       },
     });
 
-    console.log('task success');
+    console.log('task get success');
     console.log(response.data);
 
     dispatch({
@@ -60,7 +62,7 @@ export const setTask = (
   try {
     const response = await axios.post(`${API_ROOT}tasks`, { task: taskData }, { headers });
 
-    console.log('routine index');
+    console.log('task create');
     console.log(response.data);
 
     dispatch({
@@ -73,6 +75,48 @@ export const setTask = (
       payload: error,
     });
     console.log('task create errors');
+    console.log(error);
+  }
+};
+
+export const updateTask = (
+  taskid,
+  name,
+  priority,
+  goal,
+  unity,
+  routine_id,
+) => async (dispatch) => {
+  axios.defaults.headers.common.Authorization = sessionStorage.getItem('token');
+
+  const taskData = {
+    name,
+    priority,
+    goal,
+    unity,
+    routine_id,
+  };
+
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  try {
+    const response = await axios.put(`${API_ROOT}tasks/${taskid}`, { task: taskData }, { headers });
+
+    console.log('task update');
+    console.log(response.data);
+
+    dispatch({
+      type: UPDATE_TASK_SUCCESS,
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_TASK_FAIL,
+      payload: error,
+    });
+    console.log('task update errors');
     console.log(error);
   }
 };
