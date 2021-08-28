e6891bb92265fa4e07eb05b901064c891d8a0597
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-28 17:20:40 -0500

    Refactor add task

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-console */
+// import React, { useState } from 'react';
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { setTask, getAllroutines } from '../actions/tasks';
-// import { Redirect } from 'react-router-dom';
+import { Redirect, Link, useHistory } from 'react-router-dom';
+import { setTask } from '../actions/tasks';
+import AlertBox from '../component/AlertBox';
 
 const AddTask = () => {
   const { user: currentUser } = useSelector((state) => state.authentication);
@@ -18,12 +19,13 @@ const AddTask = () => {
   const [goal, setGoal] = useState();
   const [unit, setUnit] = useState();
   const [routine, setRoutine] = useState();
+  const history = useHistory();
   const routinesList = useSelector((state) => state.task.routineslist);
   const loading = useSelector((state) => state.task.loading);
-
-  useEffect(() => {
-    dispatch(getAllroutines());
-  }, [dispatch]);
+  const success = useSelector((state) => state.task.success);
+  const error = useSelector((state) => state.task.error);
+  const task = useSelector((state) => state.task.task);
+  const message = useSelector((state) => state.task.message);
 
   if (loading) {
     return <h3>Loading data ...</h3>;
@@ -33,6 +35,34 @@ const AddTask = () => {
     e.preventDefault();
     dispatch(setTask(name, priority, goal, unit, routine));
   };
+
+  useEffect(() => {
+    if (success === true) {
+      // document.getElementById('add_task_success_notif').style.display = 'block';
+      document.getElementById('add_task_success_notif').append(<AlertBox
+        alertprops={{
+          variant: 'success',
+          message,
+        }}
+      />);
+    }
+
+    if (task) {
+      setTimeout(() => {
+        history.push(`/tasks/${parseInt(task.id, 10)}`);
+      }, 5000);
+    }
+
+    if (error) {
+      // document.getElementById('add_task_success_notif').style.display = 'block';
+      document.getElementById('add_task_error_notif').append(<AlertBox
+        alertprops={{
+          variant: 'danger',
+          message: 'Oops! Something went wrong! Try again!',
+        }}
+      />);
+    }
+  }, []);
 
   const selectRoutine = (routinesList)
     ? (
@@ -46,78 +76,99 @@ const AddTask = () => {
             key={`key_${parseInt(elem.id, 10)}`}
             value={parseInt(elem.id, 10)}
           >
-            {/* <i className="fas fa-coffee" />
-              &nbsp; */}
+            <i className="fas fa-coffee fa-2x" />
+              &nbsp;
             {elem.name}
           </option>
         ))}
       </select>
     ) : (
       <select>
-        <option>
-          default
+        <option value={1}>
+          Random
         </option>
       </select>
     );
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="task_name_input">
-        Name:
-        <input
-          id="task_name_input"
-          type="text"
-          name="name"
-          placeholder="Name a Task!"
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="task_priority_input">
-        Priority:
-        <input
-          id="task_priority_input"
-          type="number"
-          name="priority"
-          min="1"
-          max="10"
-          placeholder="Give your task a priority from 1 to 10"
-          onChange={(e) => setPriority(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="task_goal_input">
-        Goal:
-        <input
-          id="task_goal_input"
-          type="number"
-          name="goal"
-          placeholder="Set your goals for this task"
-          onChange={(e) => setGoal(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="task_unity_input">
-        Unity of measurement:
-        <input
-          id="task_unity_input"
-          type="text"
-          name="Unity"
-          placeholder="Set a unity of measurement"
-          onChange={(e) => setUnit(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="task_routine_input">
-        Routine :
-        {selectRoutine}
-      </label>
-      <button
-        type="submit"
+    <div>
+      <h1>
+        Add a Task to track!
+      </h1>
+      <div
+        id="add_task_success_notif"
+        // style={{ display: 'none', color: 'green' }}
       >
-        Add Task to Track!
-      </button>
-    </form>
+        {/* successfully added */}
+      </div>
+      <div
+        id="add_task_errors_notif"
+        // style={{ display: 'none', color: 'red' }}
+      >
+        {/* Something went wrong */}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="task_name_input">
+          Name:
+          <input
+            id="task_name_input"
+            type="text"
+            name="name"
+            placeholder="Name a Task!"
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </label>
+        <label htmlFor="task_priority_input">
+          Priority:
+          <input
+            id="task_priority_input"
+            type="number"
+            name="priority"
+            min="1"
+            max="10"
+            placeholder="Give your task a priority from 1 to 10"
+            onChange={(e) => setPriority(e.target.value)}
+            required
+          />
+        </label>
+        <label htmlFor="task_goal_input">
+          Goal:
+          <input
+            id="task_goal_input"
+            type="number"
+            name="goal"
+            placeholder="Set your goals for this task"
+            onChange={(e) => setGoal(e.target.value)}
+            required
+          />
+        </label>
+        <label htmlFor="task_unity_input">
+          Unity of measurement:
+          <input
+            id="task_unity_input"
+            type="text"
+            name="Unity"
+            placeholder="Set a unity of measurement"
+            onChange={(e) => setUnit(e.target.value)}
+            required
+          />
+        </label>
+        <label htmlFor="task_routine_input">
+          Routine :
+          {selectRoutine}
+        </label>
+        <button
+          type="submit"
+        >
+          Add Task to Track!
+        </button>
+      </form>
+      <Link to="/home">
+        <i className="fas fa-arrow-left fa-2x" />
+        Back to my routines
+      </Link>
+    </div>
   );
 };
 
