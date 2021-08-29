3e7dd5b1dd983986479f26542a3c33e38bf4da97
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-28 21:06:23 -0500

    Implement measurement reducer

---
+++ src/reducers/measurement.js
@@ -0,0 +1,76 @@
+import {
+  GET_MEASUREMENT_SUCCESS,
+  GET_MEASUREMENT_FAIL,
+  SET_MEASUREMENT_SUCCESS,
+  SET_MEASUREMENT_FAIL,
+  GET_TASKS_MEASUREMENT_SUCCESS,
+  GET_TASKS_MEASUREMENT_FAIL,
+  CLEAR_EDIT_MEASUREMENT_STATE,
+} from '../actions/types';
+
+const initialState = {
+  measurement: null,
+  loading: true,
+  error: null,
+  taskslist: null,
+  edit_success: false,
+};
+
+const measurementReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_MEASUREMENT_SUCCESS:
+      return {
+        ...state,
+        measurement: action.payload.data,
+        loading: false,
+        edit_success: false,
+        error: null,
+      };
+    case GET_MEASUREMENT_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+        edit_success: false,
+      };
+    case SET_MEASUREMENT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        edit_success: true,
+        error: null,
+      };
+    case SET_MEASUREMENT_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+        edit_success: false,
+      };
+    case GET_TASKS_MEASUREMENT_SUCCESS:
+      return {
+        ...state,
+        message: '',
+        taskslist: action.payload,
+        loading: false,
+      };
+    case GET_TASKS_MEASUREMENT_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    case CLEAR_EDIT_MEASUREMENT_STATE:
+      return {
+        ...state,
+        measurement: null,
+        loading: false,
+        edit_success: false,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export default measurementReducer;
