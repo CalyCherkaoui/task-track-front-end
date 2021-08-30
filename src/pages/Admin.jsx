b85861cb3786c588d69fac3874482ae27ec4a40c
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-30 17:37:19 -0500

    Fix empty list issues with lodash

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable no-console */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect, Link, useHistory } from 'react-router-dom';
 import Spinner from 'react-bootstrap/Spinner';
+import _ from 'lodash';
+import Button from 'react-bootstrap/Button';
 import styles from '../styles/Card.module.css';
 import { getRoutines } from '../actions/routines';
+import { deleteRoutine, clearEditRoutineState } from '../actions/adminBoard';
 
 const Admin = () => {
   const { user: currentUser } = useSelector((state) => state.authentication);
@@ -20,6 +23,8 @@ const Admin = () => {
 
   const routinesList = useSelector((state) => state.routines.routines);
   const loading = useSelector((state) => state.routines.loading);
+  const deleted = useSelector((state) => state.admin.deleted);
+  const history = useHistory();
 
   if (loading) {
     return (
@@ -34,22 +39,35 @@ const Admin = () => {
     dispatch(getRoutines());
   }, [dispatch]);
 
-  const display = (list) => {
-    if (list.length === 0) {
-      return (
-        <div>
-          <h3> No routine yet! Create one!</h3>
-          <h3>
-            <i className="fas fa-coffee fa-2x" />
-          </h3>
-        </div>
-      );
+  useEffect(() => {
+    if (deleted) {
+      dispatch(getRoutines());
+      dispatch(clearEditRoutineState());
+      history.push('/admin');
+      document.getElementById('success_notif').style.display = 'block';
+      setTimeout(() => {
+        document.getElementById('success_notif').style.display = 'none';
+      }, 3000);
     }
+  }, [deleted]);
 
-    const listRoutines = list.map(
-      (element) => (
-        <li className={styles.card_wrapper} key={`key_${element.id}`}>
-          <Link to={`/routines/${parseInt(element.id, 10)}`}>
+  const handleDelete = (routineid) => {
+    console.log('delete');
+    dispatch(deleteRoutine(routineid));
+  };
+
+  const display = (list) => {
+    if (!_.isEmpty(list)) {
+      return list.map(
+        (element) => (
+          <li className={styles.card_wrapper} key={`key_${element.id}`}>
+            <Button
+              variant="outline-danger"
+            // size="lg"
+              onClick={() => handleDelete(parseInt(element.id, 10))}
+            >
+              Delete Routine
+            </Button>
             <div className={styles.card_icon}>
               <i className="fas fa-coffee fa-2x" />
             </div>
@@ -59,15 +77,18 @@ const Admin = () => {
             <div className={styles.card_title}>
               {element.attributes.priority}
             </div>
-          </Link>
-        </li>
-      ),
-    );
+          </li>
+        ),
+      );
+    }
 
     return (
-      <ul>
-        { listRoutines }
-      </ul>
+      <div>
+        <h3> No routine set yet! Create one!</h3>
+        <h3>
+          <i className="fas fa-coffee fa-2x" />
+        </h3>
+      </div>
     );
   };
 
@@ -76,6 +97,14 @@ const Admin = () => {
       <h2>
         Admin Dashboard
       </h2>
+      <div>
+        <h3>
+          <Link to="/addroutine">
+            <i className="fas fa-tools" />
+            Add new Routine
+          </Link>
+        </h3>
+      </div>
       <div>
         {display(routinesList)}
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Redirect, Link } from 'react-router-dom';
 import { IconContext } from 'react-icons';
 import { AiOutlineAreaChart } from 'react-icons/ai';
 import Spinner from 'react-bootstrap/Spinner';
+import _ from 'lodash';
 import homestyles from '../styles/Home.module.css';
 import styles from '../styles/Card.module.css';
 import { getRoutines } from '../actions/routines';
@@ -35,39 +36,33 @@ const Home = () => {
   }
 
   const display = (list) => {
-    if (list.length === 0) {
-      return (
-        <div>
-          <h3> No routine yet! Make yourself a coffee!</h3>
-          <h3>
-            <i className="fas fa-coffee fa-2x" />
-          </h3>
-        </div>
+    if (!_.isEmpty(list)) {
+      return list.map(
+        (element) => (
+          <li className={styles.card_wrapper} key={`key_${element.id}`}>
+            <Link to={`/routines/${parseInt(element.id, 10)}`}>
+              <div className={styles.card_icon}>
+                <i className="fas fa-coffee fa-2x" />
+              </div>
+              <div className={styles.card_title}>
+                {element.attributes.name}
+              </div>
+              <div className={styles.card_title}>
+                {element.attributes.priority}
+              </div>
+            </Link>
+          </li>
+        ),
       );
     }
 
-    const listRoutines = list.map(
-      (element) => (
-        <li className={styles.card_wrapper} key={`key_${element.id}`}>
-          <Link to={`/routines/${parseInt(element.id, 10)}`}>
-            <div className={styles.card_icon}>
-              <i className="fas fa-coffee fa-2x" />
-            </div>
-            <div className={styles.card_title}>
-              {element.attributes.name}
-            </div>
-            <div className={styles.card_title}>
-              {element.attributes.priority}
-            </div>
-          </Link>
-        </li>
-      ),
-    );
-
     return (
-      <ul>
-        { listRoutines }
-      </ul>
+      <div>
+        <h3> No routine set yet! Create a task!</h3>
+        <h3>
+          <i className="fas fa-coffee fa-2x" />
+        </h3>
+      </div>
     );
   };
 
