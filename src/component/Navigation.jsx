e6a68d6289e4b3d3b7b1e5f0096ec263e11556b4
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-30 18:04:34 -0500

    Update Navigations with admin links

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -1,44 +1,25 @@
 /* eslint-disable no-console */
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// import { Link, useHistory, Router } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { IconContext } from 'react-icons';
-import { BsCircleFill } from 'react-icons/bs';
 import { Navbar, Nav } from 'react-bootstrap';
 import styles from '../styles/Navigation.module.css';
-// import history from '../helpers/history';
 import { logout } from '../actions/authentication';
 
 const Navigation = () => {
-  // const history = useHistory();
-
-  // if (sessionStorage.length === 0 || sessionStorage.token === 'undefined') {
-  //   return history.push('/logout');
-  // }
-
-  const { user: currentUser } = useSelector((state) => state.authentication);
   const { isLoggedIn } = useSelector((state) => state.authentication);
-  const admin = useSelector((state) => state.authentication.admin);
   const dispatch = useDispatch();
-  // const admin = currentUser ? currentUser.data.meta.admin : false;
-  console.log(currentUser);
 
   const logOut = () => {
     dispatch(logout());
   };
 
   return (
-
-  // <Router history={history}>
-
     <Navbar className="justify-content-between">
       <Navbar.Brand href="/">
         Task & Track
         <span className={styles.nav_logo_emph}>
-          <IconContext.Provider value={{ className: 'logo_icon' }}>
-            <BsCircleFill />
-          </IconContext.Provider>
+          <i className="fab fa-creative-commons-sampling" />
         </span>
       </Navbar.Brand>
       <Nav className="">
@@ -57,16 +38,6 @@ const Navigation = () => {
             )
         }
         {
-            admin && isLoggedIn && (
-            <li className="nav-item">
-              <Link className="nav-link" to="/admin">
-                Admin-dashboard
-              </Link>
-            </li>
-            )
-          }
-
-        {
             isLoggedIn ? (
               <li className="nav-item">
                 <a href="/login" className="nav-link" onClick={logOut}>
@@ -80,7 +51,6 @@ const Navigation = () => {
                     Login
                   </Link>
                 </li>
-                <div>|</div>
                 <li className="nav-item">
                   <Link to="/signup" className="nav-link">
                     Sign-Up
@@ -91,8 +61,6 @@ const Navigation = () => {
           }
       </Nav>
     </Navbar>
-
-  // </Router>
   );
 };
 
