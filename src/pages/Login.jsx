0dc2ed51fb25bbab3d85c93cf93960ecf69e3b1a
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-31 18:51:44 -0500

    Style authentication forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,11 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 import { login } from '../actions/authentication';
 import { getAllroutines } from '../actions/tasks';
 import { getAllTasks } from '../actions/measurements';
+import styles from '../styles/Form.module.css';
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -25,46 +27,74 @@ const Login = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="login_username_input">
-        Username:
-        <input
-          id="login_username_input"
-          type="text"
-          name="username"
-          placeholder="Enter your Username"
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="login_email_input">
-        Email:
-        <input
-          id="login_email_input"
-          type="email"
-          name="email"
-          placeholder="Enter your email!"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="login_password_input">
-        Password:
-        <input
-          id="login_password_input"
-          type="password"
-          name="password"
-          placeholder="Enter your password!"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button
-        type="submit"
-      >
-        Log me in!
-      </button>
-    </form>
+    <div className={`${styles.form_wrapper} box_flex_col_centered p-4`}>
+      <Container className="g-0 shadowed_small">
+        <Row className={`${styles.form_header_wrapper} g-0`}>
+          <Col className="box_flex_col_centered g-0">
+            <p className={styles.form_header_text}>Login</p>
+          </Col>
+        </Row>
+        <Row className={`${styles.form_form_wrapper} g-0`}>
+          <Col className="box_flex_col_centered py-4">
+            <form
+              className={`${styles.form_form} px-3`}
+              onSubmit={handleSubmit}
+            >
+              <label
+                htmlFor="login_username_input"
+                className={`${styles.form_label} box_flex_col_centered`}
+              >
+                <input
+                  id="login_username_input"
+                  type="text"
+                  name="username"
+                  placeholder="Enter your username"
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  className={styles.form_input}
+                />
+              </label>
+              <label
+                htmlFor="login_email_input"
+                className={`${styles.form_label} box_flex_col_centered`}
+
+              >
+                <input
+                  id="login_email_input"
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email!"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className={styles.form_input}
+                />
+              </label>
+              <label
+                htmlFor="login_password_input"
+                className={`${styles.form_label} box_flex_col_centered`}
+              >
+                <input
+                  id="login_password_input"
+                  type="password"
+                  name="password"
+                  placeholder="Enter your password!"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className={styles.form_input}
+                />
+              </label>
+              <button
+                type="submit"
+                className={styles.form_submit}
+              >
+                Log me in!
+              </button>
+            </form>
+          </Col>
+
+        </Row>
+      </Container>
+    </div>
   );
 };
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,8 +2,10 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 import { register } from '../actions/authentication';
 import { getAllroutines } from '../actions/tasks';
+import styles from '../styles/Form.module.css';
 
 const Register = () => {
   const dispatch = useDispatch();
@@ -23,46 +25,74 @@ const Register = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="signup_username_input">
-        Username:
-        <input
-          id="signup_username_input"
-          type="text"
-          name="username"
-          placeholder="Enter your Username"
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="signup_email_input">
-        Email:
-        <input
-          id="signup_email_input"
-          type="email"
-          name="email"
-          placeholder="Enter your email!"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label htmlFor="signup_password_input">
-        Password:
-        <input
-          id="signup_password_input"
-          type="password"
-          name="password"
-          placeholder="Enter your password!"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button
-        type="submit"
-      >
-        Sign me up!
-      </button>
-    </form>
+    <div className={`${styles.form_wrapper} box_flex_col_centered p-4`}>
+      <Container className="g-0 shadowed_small">
+        <Row className={`${styles.form_header_wrapper} g-0`}>
+          <Col className="box_flex_col_centered g-0">
+            <p className={styles.form_header_text}>Sign-up</p>
+          </Col>
+        </Row>
+        <Row className={`${styles.form_form_wrapper} g-0`}>
+          <Col className="box_flex_col_centered py-4">
+            <form
+              className={`${styles.form_form} px-3`}
+              onSubmit={handleSubmit}
+            >
+              <label
+                htmlFor="signup_username_input"
+                className={`${styles.form_label} box_flex_col_centered`}
+              >
+                <input
+                  id="signup_username_input"
+                  type="text"
+                  name="username"
+                  placeholder="Enter your username"
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  className={styles.form_input}
+                />
+              </label>
+              <label
+                htmlFor="signup_email_input"
+                className={`${styles.form_label} box_flex_col_centered`}
+
+              >
+                <input
+                  id="signup_email_input"
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email!"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className={styles.form_input}
+                />
+              </label>
+              <label
+                htmlFor="signup_password_input"
+                className={`${styles.form_label} box_flex_col_centered`}
+              >
+                <input
+                  id="signup_password_input"
+                  type="password"
+                  name="password"
+                  placeholder="Enter your password!"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className={styles.form_input}
+                />
+              </label>
+              <button
+                type="submit"
+                className={styles.form_submit}
+              >
+                Sign me up!
+              </button>
+            </form>
+          </Col>
+
+        </Row>
+      </Container>
+    </div>
   );
 };
 
