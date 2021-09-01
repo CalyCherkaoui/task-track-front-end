bc9bb11b5430a2b73bcf36df5d33610a7bee9e5a
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-08-31 19:26:48 -0500

    Style add task form

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -4,9 +4,11 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
 import Spinner from 'react-bootstrap/Spinner';
+import { Container, Row, Col } from 'react-bootstrap';
 import { setTask, clearEditTaskState } from '../actions/tasks';
 import { getAllTasks } from '../actions/measurements';
 import { setNotification, clearNotification } from '../actions/notifications';
+import styles from '../styles/Form.module.css';
 
 const AddTask = () => {
   const { user: currentUser } = useSelector((state) => state.authentication);
@@ -66,6 +68,7 @@ const AddTask = () => {
         name="routine"
         id="routine_id"
         onChange={(e) => setRoutine_id(e.target.value)}
+        className={styles.form_input}
       >
         <option
           key="key_"
@@ -82,7 +85,10 @@ const AddTask = () => {
         ))}
       </select>
     ) : (
-      <select>
+      <select
+        className={styles.form_input}
+
+      >
         <option value={1}>
           No routine available yet!
         </option>
@@ -90,72 +96,108 @@ const AddTask = () => {
     );
 
   return (
-    <div>
-      <h1>
-        Add a Task to track!
-      </h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="task_name_input">
-          Name:
-          <input
-            id="task_name_input"
-            type="text"
-            name="name"
-            placeholder="Name a Task!"
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </label>
-        <label htmlFor="task_priority_input">
-          Priority:
-          <input
-            id="task_priority_input"
-            type="number"
-            name="priority"
-            min="1"
-            max="10"
-            placeholder="Give your task a priority from 1 to 10"
-            onChange={(e) => setPriority(e.target.value)}
-            required
-          />
-        </label>
-        <label htmlFor="task_goal_input">
-          Goal:
-          <input
-            id="task_goal_input"
-            type="number"
-            name="goal"
-            placeholder="Set your goals for this task"
-            onChange={(e) => setGoal(e.target.value)}
-            required
-          />
-        </label>
-        <label htmlFor="task_unity_input">
-          Unity of measurement:
-          <input
-            id="task_unity_input"
-            type="text"
-            name="Unity"
-            placeholder="Set a unity of measurement"
-            onChange={(e) => setUnit(e.target.value)}
-            required
-          />
-        </label>
-        <label htmlFor="task_routine_input">
-          Routine :
-          {selectRoutine}
-        </label>
-        <button
-          type="submit"
-        >
-          Add Task to Track!
-        </button>
-      </form>
-      <Link to="/home">
-        <i className="fas fa-arrow-left fa-2x" />
-        Back to my routines
-      </Link>
+
+    <div className={`${styles.form_wrapper} box_flex_col_centered p-4`}>
+      <Container className="g-0 shadowed_small">
+        <Row className={`${styles.form_header_wrapper} g-0`}>
+          <Col className="box_flex_col_centered g-0">
+            <p className={styles.form_header_text}>
+              Add a Task to track!
+            </p>
+          </Col>
+        </Row>
+        <Row className={`${styles.form_form_wrapper} g-0`}>
+          <Col className="box_flex_col_centered py-4">
+            <form
+              onSubmit={handleSubmit}
+              className={`${styles.form_form} px-3`}
+            >
+              <label
+                htmlFor="task_name_input"
+                className={`${styles.form_label} box_flex_col_centered`}
+              >
+                Name:
+                <input
+                  id="task_name_input"
+                  type="text"
+                  name="name"
+                  placeholder="Name a Task!"
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                  className={styles.form_input}
+                />
+              </label>
+              <label
+                className={`${styles.form_label} box_flex_col_centered`}
+                htmlFor="task_priority_input"
+              >
+                Priority:
+                <input
+                  id="task_priority_input"
+                  type="number"
+                  name="priority"
+                  min="1"
+                  max="10"
+                  placeholder="Give your task a priority from 1 to 10"
+                  onChange={(e) => setPriority(e.target.value)}
+                  required
+                  className={styles.form_input}
+                />
+              </label>
+              <label
+                className={`${styles.form_label} box_flex_col_centered`}
+                htmlFor="task_goal_input"
+              >
+                Goal:
+                <input
+                  id="task_goal_input"
+                  type="number"
+                  name="goal"
+                  placeholder="Set your goals for this task"
+                  onChange={(e) => setGoal(e.target.value)}
+                  required
+                  className={styles.form_input}
+                />
+              </label>
+              <label
+                className={`${styles.form_label} box_flex_col_centered`}
+                htmlFor="task_unity_input"
+              >
+                Unity of measurement:
+                <input
+                  id="task_unity_input"
+                  type="text"
+                  name="Unity"
+                  placeholder="Set a unity of measurement"
+                  onChange={(e) => setUnit(e.target.value)}
+                  required
+                  className={styles.form_input}
+                />
+              </label>
+              <label
+                className={`${styles.form_label} box_flex_col_centered`}
+                htmlFor="task_routine_input"
+              >
+                {selectRoutine}
+              </label>
+              <button
+                className={styles.form_submit}
+                type="submit"
+              >
+                Add Task to Track!
+              </button>
+            </form>
+          </Col>
+        </Row>
+        <Row>
+          <Link to="/home">
+            <i className="fas fa-arrow-left fa-2x" />
+            Back to my routines
+          </Link>
+        </Row>
+      </Container>
     </div>
+
   );
 };
 
