4ceb60c55c6cfd4b382ec2558bdf31a05869de50
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-09-01 16:25:40 -0500

    Style admin page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -5,7 +5,9 @@ import { Redirect, Link, useHistory } from 'react-router-dom';
 import Spinner from 'react-bootstrap/Spinner';
 import _ from 'lodash';
 import Button from 'react-bootstrap/Button';
-import styles from '../styles/Card.module.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import cardstyles from '../styles/Card.module.css';
+import adminstyles from '../styles/Dashboard.module.css';
 import { getRoutines } from '../actions/routines';
 import { deleteRoutine, clearEditRoutineState } from '../actions/adminBoard';
 
@@ -60,55 +62,74 @@ const Admin = () => {
     if (!_.isEmpty(list)) {
       return list.map(
         (element) => (
-          <li className={styles.card_wrapper} key={`key_${element.id}`}>
-            <Button
-              variant="outline-danger"
-            // size="lg"
-              onClick={() => handleDelete(parseInt(element.id, 10))}
-            >
-              Delete Routine
-            </Button>
-            <div className={styles.card_icon}>
-              <i className={element.attributes.icon} />
-            </div>
-            <div className={styles.card_title}>
-              {element.attributes.name}
-            </div>
-            <div className={styles.card_title}>
-              {element.attributes.priority}
-            </div>
-          </li>
+          <div className={`${cardstyles.card_wrapper} p-2 ratio ratio-1x1 m-2 shadowed_small`} key={`key_${element.id}`}>
+            <Link to={`/routines/${parseInt(element.id, 10)}`} className="flex-column d-flex">
+              <div className={`${cardstyles.card_icon} ratio-1x1 p-2 box_flex_col_centered`}>
+                <i className={element.attributes.icon} />
+              </div>
+              <div className={`${cardstyles.card_minimalist_text} my-2 box_flex_col_centered`}>
+                <span className="align-self-center">{element.attributes.name}</span>
+              </div>
+              <Button
+                variant="danger"
+                onClick={() => handleDelete(parseInt(element.id, 10))}
+                className="g-0"
+              >
+                Delete Routine
+              </Button>
+            </Link>
+          </div>
         ),
       );
     }
 
     return (
-      <div>
-        <h3> No routine set yet! Create one!</h3>
-        <h3>
-          <i className="fas fa-coffee fa-2x" />
-        </h3>
+      <div className="w-70">
+        <Container>
+          <Row className={`${cardstyles.list_header_wrapper} d-flex align-items-baseline`}>
+            <Col className={`box_flex_col_centered ${cardstyles.list_header_icon}`} xs={2}>
+              <span>
+                <i className="fas fa-coffee fa-1x" />
+              </span>
+            </Col>
+            <Col className="box_flex_col_centered g-0" xs={10}>
+              <Link to="/addroutine" className={cardstyles.list_header_text}>
+                No routine set yet!
+                Create one!
+              </Link>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   };
 
   return (
-    <div className="container">
-      <h2>
-        Admin Dashboard
-      </h2>
-      <div>
-        <h3>
-          <Link to="/addroutine">
-            <i className="fas fa-tools" />
-            Add new Routine
+    <Container>
+      <Row className={`${cardstyles.list_header_wrapper} g-0 d-flex align-items-center`}>
+        <Col className="box_flex_col_centered g-0">
+          <Link to="/home" className={cardstyles.list_header_text}>
+            Admin Dashboard
           </Link>
-        </h3>
-      </div>
-      <div>
+        </Col>
+      </Row>
+      <Row className={`${adminstyles.tool_header_wrapper} d-flex align-items-baseline`}>
+        <Col className={`box_flex_col_centered ${adminstyles.tool_header_icon}`} xs={3}>
+          <span>
+            <i className="fas fa-tools" />
+          </span>
+        </Col>
+        <Col className="box_flex_col_centered g-0" xs={9}>
+          <Link to="/addroutine" className={adminstyles.tool_header_text}>
+            Add new Routine
+            <i className="fas fa-plus-circle" />
+          </Link>
+        </Col>
+      </Row>
+      <Row className="d-flex flex-wrap">
         {display(routinesList)}
-      </div>
-    </div>
+      </Row>
+    </Container>
   );
 };
 
