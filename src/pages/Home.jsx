939db7e78211fe2de837cd96cd986df7568cafac
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-09-01 16:25:49 -0500

    Style home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,10 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
-import { IconContext } from 'react-icons';
-import { AiOutlineAreaChart } from 'react-icons/ai';
 import Spinner from 'react-bootstrap/Spinner';
 import _ from 'lodash';
-import homestyles from '../styles/Home.module.css';
-import styles from '../styles/Card.module.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import cardstyles from '../styles/Card.module.css';
 import { getRoutines } from '../actions/routines';
 
 const Home = () => {
@@ -39,47 +37,56 @@ const Home = () => {
     if (!_.isEmpty(list)) {
       return list.map(
         (element) => (
-          <li className={styles.card_wrapper} key={`key_${element.id}`}>
-            <Link to={`/routines/${parseInt(element.id, 10)}`}>
-              <div className={styles.card_icon}>
+          <div className={`${cardstyles.card_wrapper} p-2 ratio ratio-1x1 m-2 shadowed_small`} key={`key_${element.id}`}>
+            <Link to={`/routines/${parseInt(element.id, 10)}`} className="flex-column d-flex">
+              <div className={`${cardstyles.card_icon} ratio-1x1 p-2 box_flex_col_centered`}>
                 <i className={element.attributes.icon} />
               </div>
-              <div className={styles.card_title}>
-                {element.attributes.name}
-              </div>
-              <div className={styles.card_title}>
-                {element.attributes.priority}
+              <div className={`${cardstyles.card_minimalist_text} my-2 box_flex_col_centered`}>
+                <span className="align-self-center">{element.attributes.name}</span>
               </div>
             </Link>
-          </li>
+          </div>
         ),
       );
     }
 
     return (
-      <div>
-        <h3> No routine set yet! Create a task!</h3>
-        <h3>
-          <i className="fas fa-coffee fa-2x" />
-        </h3>
+
+      <div className="w-70">
+        <Container>
+          <Row className={`${cardstyles.list_header_wrapper} d-flex align-items-baseline`}>
+            <Col className={`box_flex_col_centered ${cardstyles.list_header_icon}`} xs={2}>
+              <span>
+                <i className="fas fa-coffee fa-1x" />
+              </span>
+            </Col>
+            <Col className="box_flex_col_centered g-0" xs={10}>
+              <Link to="/addtask" className={cardstyles.list_header_text}>
+                No routine set yet!
+                Create a task!
+              </Link>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   };
 
   return (
-    <div className="container">
-      <h2>
-        My daily Routines
-        <span className={homestyles.title_icon}>
-          <IconContext.Provider value={{ className: 'footer_icon' }}>
-            <AiOutlineAreaChart />
-          </IconContext.Provider>
-        </span>
-      </h2>
-      <div>
+    <Container className="g-0 my-2">
+      <Row className={`${cardstyles.list_header_wrapper} g-0 my-2 d-flex align-items-center`}>
+        <Col className="box_flex_col_centered g-0">
+          <Link to="/addtask" className={cardstyles.list_header_text}>
+            My daily Routines
+          </Link>
+        </Col>
+      </Row>
+      <Row className="d-flex flex-wrap">
         {display(routinesList)}
-      </div>
-    </div>
+      </Row>
+    </Container>
+
   );
 };
 
