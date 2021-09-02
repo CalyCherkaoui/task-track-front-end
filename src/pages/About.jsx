f998260c39543fde6a902ad8d56f2b8cf70b7c03
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-09-02 18:20:22 -0500

    Update styling of about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,28 +1,35 @@
+/* eslint-disable max-len */
 import React from 'react';
-import { IconContext } from 'react-icons';
-import { AiFillGithub } from 'react-icons/ai';
-import { FiLinkedin } from 'react-icons/fi';
+import { SiRails } from 'react-icons/si';
 
 import styles from '../styles/AboutPage.module.css';
 
 const About = () => (
   <div className={styles.aboutpage_wrapper}>
-    <div className={`${styles.about_frame} box_flex_col_centered m-4 ratio ratio-1x1`}>
-      <h3 className={styles.aboutpage_text}>
-        This is a tacking app taht allows you to track your daily routine tasks.
-        <br />
-        This Application was built with Ruby On Rails for the Back-end and React for the Front-end.
-      </h3>
-      <div className={styles.about_icons}>
+    <div className={`${styles.about_frame} d-flex justify-content-between flex-column m-4`}>
+      <div className={styles.aboutpage_text}>
+        <p>This is a tacking app that allows you to track your daily routine tasks.</p>
+        <p>This Application was built with Ruby On Rails for the Back-end and React for the Front-end.</p>
+      </div>
+      <div className="d-flex flex-row justify-content-center align-items-center">
+        <span>Built with:</span>
+        <div className={styles.about_icons}>
+          <i className="fab fa-react" />
+        </div>
+        <div className={styles.about_icons}>
+          <SiRails />
+        </div>
+      </div>
+      <div className="d-flex flex-row">
         <a href="https://github.com/CalyCherkaoui/task-track-front-end" className={styles.about_icons_link}>
-          <IconContext.Provider value={{ className: `${styles.about_icon}` }}>
-            <AiFillGithub />
-          </IconContext.Provider>
+          <div className={styles.about_icons}>
+            <i className="fab fa-github" />
+          </div>
         </a>
         <a href="https://www.linkedin.com/in/houda-cherkaoui-64106395/" className={styles.about_icons_link}>
-          <IconContext.Provider value={{ className: `${styles.about_icon}` }}>
-            <FiLinkedin />
-          </IconContext.Provider>
+          <div className={styles.about_icons}>
+            <i className="fab fa-linkedin-in" />
+          </div>
         </a>
       </div>
     </div>
