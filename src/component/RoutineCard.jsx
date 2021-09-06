3146b4ca2866f5f0b1cca31ddefaaa6d11087e1d
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-09-06 16:36:07 -0500

    Update Routine Card

--- src/component/RoutineCard.jsx
+++ src/component/RoutineCard.jsx
@@ -1,24 +1,20 @@
-/* eslint-disable no-console */
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import styles from '../styles/Card.module.css';
 
-const RoutineCard = ({ routine }) => {
-  console.log('routines index');
-  return (
-    <div className={styles.card_wrapper}>
-      <div className={styles.card_icon}>
-        <i className={`${routine.icon} fa-2x`} />
-      </div>
-      <div className={styles.card_title}>
-        {routine.name}
-      </div>
-      <div className={styles.card_title}>
-        {routine.priority}
-      </div>
+const RoutineCard = ({ routine }) => (
+  <div className={styles.card_wrapper}>
+    <div className={styles.card_icon}>
+      <i className={`${routine.icon} fa-2x`} />
     </div>
-  );
-};
+    <div className={styles.card_title}>
+      {routine.name}
+    </div>
+    <div className={styles.card_title}>
+      {routine.priority}
+    </div>
+  </div>
+);
 
 RoutineCard.propTypes = {
   routine: PropTypes.shape(
